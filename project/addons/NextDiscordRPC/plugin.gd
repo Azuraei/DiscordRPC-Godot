832e69eb8ccafe65f76dd6f68ec4fb0e094ae8a5
MiraDiv-git <omega24122004@gmail.com>
2026-05-26 13:35:31 +0300

    Auto copy libs function
    Added function to plugin.gd, that automatically copies discord_partner_sdk and NextDiscordRPC templates to export folder

--- project/addons/NextDiscordRPC/plugin.gd
+++ project/addons/NextDiscordRPC/plugin.gd
@@ -1,6 +1,9 @@
 @tool
 extends EditorPlugin
 
+var export_plugin: EditorExportPlugin
+
+
 func _enter_tree() -> void:
 	if not ProjectSettings.has_setting("discord_rpc/application_id"):
 		ProjectSettings.set_setting("discord_rpc/application_id", "0")
@@ -11,7 +14,24 @@ func _enter_tree() -> void:
 		"type": TYPE_STRING,
 		"hint": PROPERTY_HINT_NONE,
 	})
+	
+	export_plugin = DiscordRPCExportPlugin.new()
+	add_export_plugin(export_plugin)
+	
 	add_autoload_singleton("Discord", "res://addons/NextDiscordRPC/discord_rpc_autoload.gd")
 
+
 func _exit_tree() -> void:
+	remove_export_plugin(export_plugin)
 	remove_autoload_singleton("Discord")
+
+
+class DiscordRPCExportPlugin extends EditorExportPlugin:
+	func _get_name() -> String:
+		return "NextDiscordRPC"
+	
+	func _export_begin(features: PackedStringArray, is_debug: bool, path: String, flags: int) -> void:
+		if "linux" in features or "linuxbsd" in features:
+			add_shared_object("res://addons/NextDiscordRPC/bin/linux/libdiscord_partner_sdk.so", PackedStringArray(), "")
+		elif "windows" in features:
+			add_shared_object("res://addons/NextDiscordRPC/bin/windows/discord_partner_sdk.dll", PackedStringArray(), "")
